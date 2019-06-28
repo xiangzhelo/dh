c8e5563526c8df74161772fe2baec93005c8add0
翔着咯 <949460716@qq.com>
2019-06-28 21:49:32 +0800

    py

---
+++ 1.py
@@ -0,0 +1,19 @@
+import os
+import sqlite3
+import requests
+from win32crypt import CryptUnprotectData
+
+path1 = r"C:\Users\Administrator\AppData\Local\Google\Chrome\User Data\Default"
+sql = "select * from cookies where host_key like '%.aliexpress.com'"
+conn = sqlite3.connect(path1)
+conn.row_factory = sqlite3.Row
+cu = conn.cursor()
+cursor = cu.execute(sql)
+cookie = ''
+for row in cursor:
+    cookie = cookie + row['name'] + r"=" + CryptUnprotectData(row['encrypted_value'])[1].decode() + r";"
+    #fo=open('d:\\test.txt','w')
+    #fo.write(CryptUnprotectData(row['encrypted_value'])[1].decode())
+    #fo.close()
+conn.close()
+print(cookie)
